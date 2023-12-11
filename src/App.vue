6e9b0e5ce75d4dcdf6afe676e2d83ddda7410123
da-takatsuki <da-takatsuki@kddi.com>
2023-12-11 14:11:45 +0900

    Add initial project files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Counter from "./components/Counter.vue";
+</script>
+
+<template>
+  <div class="body"><Counter /></div>
+</template>
+
+<style scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #e8f2fe;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="box">
+    <div id="title">Vue Counter</div>
+    <p id="count">{{ count }}</p>
+    <div class="_button">
+      <button id="_increment_button" @click="increment">+</button>
+      <button id="_decrement_button" @click="decrement">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  width: 300px;
+  height: 400px;
+  background-color: #fff;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.button {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+#_increment_button {
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+  background-color: #1c5cd0;
+  border-color: #1c5cd0;
+  color: #fff;
+  margin: 0px 20px;
+  font-size: 24px;
+}
+
+#_decrement_button {
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+  background-color: #fff;
+  border-color: #1c5cd0;
+  color: #1c5cd0;
+  margin: 0px 20px;
+  font-size: 24px;
+}
+
+#title {
+  font-size: 24px;
+  font-weight: bold;
+  margin: 12px;
+  color: #aaa;
+}
+
+#count {
+  font-size: 48px;
+  font-weight: bold;
+  margin: 60px 0px;
+  color: #1c5cd0;
+}
+</style>
